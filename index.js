fbc5314a808d09268358c75ab31e95c820d0b56a
Haley Townsend <haleyrk@hotmail.com>
2018-04-10 10:49:07 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const API_BASE_URL = 'https://swapi.co/api/people/'
+
+var swapi = {}
+
+function getApi() {
+  return jQuery
+}
+
+
+
+
+$('.btnClick').submit(function (ev) {
+  ev.preventDefault();
+  character = $('#characterSearch').val()
+  console.log(character);
+  //send to next function
+})
